0c820e28b555414ba226ca391ccdde738e140091
ktkakadiya <ktkakadiya@gmail.com>
2016-09-01 12:44:52 +0530

    lab login added

---
+++ Database_Demo/Database_Demo/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Database_Demo.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Database_Demo/Database_Demo/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Database_Demo.Startup))]
+namespace Database_Demo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
